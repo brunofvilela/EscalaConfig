a68a07817f75287aa3df94667d6c9febb61d212c
Bruno Vilela <bruno.vilela@syonet.com>
2025-12-05 15:44:52 -0300

    correção

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,13 +1,26 @@
-// Firebase Config (substitua pelo seu)
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "escalaconfig.firebaseapp.com",
-  projectId: "escalaconfig",
-  storageBucket: "escalaconfig.firebasestorage.app",
-  messagingSenderId: "393446307606",
-  appId: "1:393446307606:web:69f1f038edf659739e8237",
-  measurementId: "G-160CC381TP"
-};
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } 
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+    const firebaseConfig = {
+      apiKey: "SUA_API_KEY_AQUI",
+      authDomain: "escalaconfig.firebaseapp.com",
+      projectId: "escalaconfig",
+      storageBucket: "escalaconfig.firebasestorage.app",
+      messagingSenderId: "393446307606",
+      appId: "1:393446307606:web:69f1f038edf659739e8237",
+      measurementId: "G-160CC381TP"
+    };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+// Re-exportar funções do Firestore
+export {
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    doc
+};
